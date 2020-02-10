61c20dfe2b41cb6f13cbc2eee4590b405f378c87
Dorian Calin <dorin.m.calin@gmail.com>
2020-02-10 16:28:15 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,7 +4,7 @@ end
 
 def find_max_value(array)
   x = 0
-  array.lenght.times do |index|
+  array.length.times do |index|
   if array[index] > x
     x = array[index]
   end
@@ -14,7 +14,7 @@ end
 
 def find_min_value(array)
   x = 0
-  array.lenght.times do |index|
+  array.length.times do |index|
   if array[index] < x
     x = array[index]
   end
